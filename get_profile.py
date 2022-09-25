4a92fd0ca72f8b2b38aa73b02cd6059aa46a98a1
Johnny Ventura <108906793+J8ventura@users.noreply.github.com>
2022-09-25 18:26:49 -0500

    Update get_profile.py

--- get_profile.py
+++ get_profile.py
@@ -23,21 +23,3 @@ def get_profile(username: str) -> dict:
         }
     )
     return profile     
-        
-
-
-        
-#testing
-def test_update_profile():
-    test_event = {
-        'requestContext': {
-            'identity': {
-                'cognitoIdentityId': '0893be13-3d6c-489e-9029-63e4b28945cb'
-            }
-        }
-    }
-
-    print(lambda_handler(test_event, {}))
-
-test_update_profile()
-
